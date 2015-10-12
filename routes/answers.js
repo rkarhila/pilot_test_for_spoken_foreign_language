9ea47c6d288b869fbbf46a77bd870839530d109b
rkarhila <reima.karhila@aalto.fi>
2015-10-12 22:32:41 +0300

    forgot something

---
+++ routes/answers.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+var exec = require('child_process').exec;
+
+
+router.get('/video/:video', function(req, res, next) {
+
+    if (req.user.role == 'user') {
+	res.json({msg: 'No privileges for viewing'});
+    }
+    else {
+	var file = './uploads/encoded_video/'+req.params.video;
+	var range = req.headers.range;
+	var positions = range.replace(/bytes=/, "").split("-");
+	var start = parseInt(positions[0], 10);
+
+	fs.stat(file, function(err, stats) {
+	    var total = stats.size;
+	    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+	    var chunksize = (end - start) + 1;
+
+	    res.writeHead(206, {
+		"Content-Range": "bytes " + start + "-" + end + "/" + total,
+		"Accept-Ranges": "bytes",
+		"Content-Length": chunksize,
+		"Content-Type": "video/mp4"
+	    });
+
+	    var stream = fs.createReadStream(file, { start: start, end: end })
+		.on("open", function() {
+		    stream.pipe(res);
+		}).on("error", function(err) {
+		    res.end(err);
+		});
+	});
+    }
+});
+
+
+module.exports = router;
